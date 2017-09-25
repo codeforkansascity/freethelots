3453aaffef2636a238a6a6c31761cb1947e275d5
matt <matt@ontargetinteractive.com>
2017-09-24 20:35:28 -0500

    moved index code to welcome view moved css and png to public folder

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -1,95 +1,130 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Free The Lots</title>
+    <meta name="description" content="Identifies which properties have extra det attached to the sale, and list the associated claimants and amounts owed, assisting the city, country, developers, and buyers.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Bootswatch Styling for Improving the Aesthetics -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/flatly/bootstrap.min.css">
+    <!-- Font Awesome CSS Icons (For cool glyphicons) -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Local Style Sheet -->
+    <link rel="stylesheet" href="style.css">
+</head>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+<body>
+<!--[if lt IE 7]>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<!-- Main Bootstrap Search -->
+<div class="container">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Jumbotron for Title -->
+    <div class="jumbotron" style="background-color: #20315A; color: white;">
+        <h1 class="text-center"><span class="text-info glyphicon glyphicon-home"></span><span class="text-info strong"> Free The Lots!</span>
+            <span class="text-info glyphicon glyphicon-home"></span>
+        </h1>
+    </div>
+    <!-- /.jumbotron -->
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+    <div class="row">
+        <div class="col-sm-12">
+            <br>
+            <!-- This panel will initially be made up of a panel and wells for each of the retrieved Properties-->
+            <div class="panel panel-warning">
+                <!-- Panel Heading for the retrieved property data box -->
+                <div class="panel-heading">
+                    <h3 class="panel-title"><strong>Search Options</strong></h3>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="row search-row">
+                    <div class="col-lg-12">
+                        <div class="input-group">
+                                <span class="input-group-btn">
+                      <button class="btn btn-info" type="button"><span class="glyphicon glyphicon-search"></span>                                Search!</button>
+                                </span>
+                            <input type="text" class="form-control" placeholder="Search for properties by: Lender name / Busisness Name">
+                        </div>
+                        <!-- /input-group -->
+                    </div>
+                    <!-- /.col-lg-12 -->
                 </div>
+                <!-- /.row -->
+                <div class="row search-row">
+                    <div class="col-lg-12">
+                        <div class="input-group">
+                                <span class="input-group-btn">
+                          <button class="btn btn-info" type="button"><span class="glyphicon glyphicon-search"></span>                                Search!</button>
+                                </span>
+                            <input type="text" class="form-control" placeholder="Search for properties by: Individual Name [First, Last]">
+                        </div>
+                        <!-- /input-group -->
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+                <div class="btn-group" role="group" aria-label="...">
+                    <button type="button" class="btn btn-info btn-search">Claimants by Number of Claims</button>
+                    <!-- <button type="button" class="btn btn-info btn-search">Busisness Name</button> -->
+                    <!-- <button type="button" class="btn btn-info btn-search">Right</button> -->
+                </div>
+            </div>
+            <!-- /panel -->
+        </div>
+        <!-- /col -->
+    </div>
+    <!-- /row -->
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <!-- This row will handle all of the retrieved property data -->
+    <div class="row">
+        <div class="col-sm-12">
+            <br>
+            <!-- This panel will initially be made up of a panel and wells for each of the retrieved Properties-->
+            <div class="panel panel-success">
+                <!-- Panel Heading for the retrieved property data box -->
+                <div class="panel-heading">
+                    <h3 class="panel-title"><strong>Property Info!</strong></h3>
+                </div>
+                <!-- This main panel will hold each of the resulting property's data -->
+                <div class="panel-body" id="well-section">
+                    <div class="row header-row">
+                        <div class="col-xs-3 property-header">Claimant</div>
+                        <div class="col-xs-2 text-center property-header">Date of Claim</div>
+                        <div class="col-xs-3 text-center property-header">Document Type</div>
+                        <div class="col-xs-4 text-center property-header">Legal Description</div>
+                    </div>
+                </div>
+                <div class="panel-body" id="property-container">
+                    <!-- DOM -->
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+
+    <!-- Footer Region -->
+    <div class="row">
+        <div class="col-sm-12">
+            <!-- Line Break followed by closing -->
+            <hr>
+            <h5 class="text-center"><small>Made by Doodle with lots and lots of <i class="fa fa-heart"></i></small></h5>
+        </div>
+    </div>
+</div>
+
+<!-- jQuery JS -->
+<script src="https://code.jquery.com/jquery.js"></script>
+<!-- Code to a JavaScript File -->
+<!-- script src="app.js"></script -->
+
+</body>
+
 </html>
