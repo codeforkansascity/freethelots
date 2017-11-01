2eea8345076d921fedd0ac49574bb34323542d66
matsplat <matthewjohncoleman@gmail.com>
2017-11-01 00:19:07 -0500

    add test queries for matching legal description

--- app/routes/web.php
+++ app/routes/web.php
@@ -25,6 +25,65 @@ Route::get('/parties/search/{search}', 'PartyController@search');
 Route::get('/parcels', 'ParcelController@index');
 Route::get('/parcels/search/{search}', 'ParcelController@searchGrantor');
 
+Route::get('/test', function (){
+
+    $time_start = microtime(true);
+    // get all records of single property
+
+    $low = rand(1, 1000);
+    $legal = DB::table('landbank')
+        ->select('*')
+        ->where('id',$low)
+        ->first();
+
+    if(empty($legal)){
+        dd('empty');
+    }
+
+    $parts = explode( ';', $legal->combined_legal);
+    $parsed = '';
+    foreach($parts as $part){
+
+        $first = substr(trim($part), 0, 3);
+
+        if(in_array($first, [ 'SBD', 'LT ', 'BLK'])){
+            $parsed .= $part.';';
+        }
+    }
+    $parsed = rtrim($parsed, ';');
+
+
+    $regular_count = DB::table('deeds')
+        ->select('combined_legal')
+        ->where('combined_legal',  $legal->combined_legal)
+        ->orderby('date_received')
+        ->get();
+
+
+    $parsed_count =  DB::table('deeds')
+        ->select('combined_legal')
+        ->where('combined_legal', 'like', '%'.$parsed.'%')
+        ->get();
+
+    $extra_results = $parsed_count->count() - $regular_count->count();
+    $time_end = microtime(true);
+    $time = round($time_end - $time_start, 2). ' sec';
+
+    return compact( 'time','parsed', 'extra_results' , 'legal' ,'regular_count', 'parsed_count');
+});
+
+Route::get('/landbank', function (){
+    // get only land bank deeds
+    $deeds =  DB::table('deeds')
+        ->select('combined_legal')
+        ->where('grantee',  'like', 'LAND BANK%')
+        ->groupby('combined_legal')
+        ->get();
+
+
+    return $deeds;
+});
+
 
 
 
