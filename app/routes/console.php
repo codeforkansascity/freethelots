2c986be75ce385e9a85a6ede4315b7496706f9bc
matt <matt@ontargetinteractive.com>
2018-02-26 18:39:05 -0600

    alter search queries to group transfers and mortgages test routes added for mortgages and transfers

--- app/routes/console.php
+++ app/routes/console.php
@@ -16,3 +16,41 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('sample-transfers', function(){
+
+    $parcels = \App\Parcel::inRandomOrder()->limit(10)->get();
+
+    foreach($parcels as $parcel){
+
+        $transfers = $parcel->transfers()->get();
+        $tcount = count($transfers);
+
+        $mortgages = $parcel->mortgages();
+        $mcount = count($mortgages);
+
+        $file = fopen('/home/vagrant/Code/freethelots/test-data/parcel-'.$parcel->id.".txt", 'w');
+
+        fwrite($file, PHP_EOL.'Parcel '.PHP_EOL.str_repeat("-=", 40).PHP_EOL.PHP_EOL);
+
+        fwrite($file, json_encode($parcel));
+        fwrite($file, PHP_EOL.PHP_EOL);
+
+        fwrite($file, PHP_EOL.'Parcel Transfers '.PHP_EOL.str_repeat("-=", 40).PHP_EOL.PHP_EOL);
+        foreach ($transfers as $transfer){
+            fwrite($file, json_encode($transfer));
+            fwrite($file, PHP_EOL.PHP_EOL);
+        }
+        fwrite($file, PHP_EOL.'Active Mortgages '.PHP_EOL.str_repeat("-=", 40).PHP_EOL.PHP_EOL);
+        foreach($mortgages as $mortgage){
+            fwrite($file, json_encode($mortgage));
+            fwrite($file, PHP_EOL.PHP_EOL);
+
+        }
+        fclose($file);
+
+    }
+
+
+    //return compact('parcel', 'tcount', 'transfers','mcount','mortgages');
+});
